8e3a69fa867556a7e6de707e0b3262abcdd68b0b
Stephen <SVela@my.campus.edu>
2026-09-15 20:39:31 -0500

    Updates

--- ProjectMilestone.js
+++ ProjectMilestone.js
@@ -24,7 +24,7 @@ navList.style.display = isHidden ? 'flex' : 'none'
 menuBtn.setAttribute('aria-expanded', isHidden ? 'true' : 'false')
 })
 
-navContainer.parentNode.insertBefore(menuBtn, navContainer)
+navContainer.insertBefore(menuBtn, navList)
 }
 
 // Part 2
